244afb64e364efcaa66344d97bba7f497e6b898e
Knight125 <karanicoleknight@gmail.com>
2018-08-29 13:34:15 -1000

    updated to the create a note page

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "uploads-resources"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://og89p565k3.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_JviF6iiW2",
+    APP_CLIENT_ID: "vqj2bop5ibamrnngeme10ncgt",
+    IDENTITY_POOL_ID: "us-east-1:e7299804-8c9a-4c2c-8318-bffbee75e007"
+  }
+};
